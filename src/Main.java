f1922b97bedc1a414c59707ae8783223dbd6f147
cnrtrgt1453 <canerturgut34@gmail.com>
2019-09-29 14:42:22 +0300

    foreach döngüsü, myList arrayinde number değişkeninin dolaşması

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String ogrenci1="Caner";
-        String ogrenci2="Osman";
-        String ogrenci3="Turgut";
+        double[] myList={1.2,1.3,1.4,1.5};
 
-
-        System.out.println(ogrenci1);
-        System.out.println(ogrenci2);
-        System.out.println(ogrenci3);
-
-        System.out.println("----------------------");
-        String[] ogrenciler=new String[3];
-        ogrenciler[0]="x";
-        ogrenciler[1]="y";
-        ogrenciler[2]="z";
-
-        for (int i=0;i<ogrenciler.length;i++){
-            System.out.println(ogrenciler[i]);
+        double total=0;
+        double max=myList[0];
+        for (double number:myList){
+            if(max<number){
+                max=number;
+            }
+            total=total+number;
+            System.out.println(number);
         }
-
-        System.out.println("------------------------");
-
-        //foreach kullanımı...
-        for (String ogrenci:ogrenciler){
-            System.out.println(ogrenci);
-        }
+        System.out.println("Toplam= "+total);
+        System.out.println("En büyük: "+max);
     }
 }
